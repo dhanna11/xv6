eb44d35675f76d18108c335f25ea4fe0cd411960
David Hanna <david.hanna11@gmail.com>
2022-08-19 22:14:30 -0400

    Fix memory leak

--- kernel/proc.c
+++ kernel/proc.c
@@ -164,6 +164,9 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+  if(p->alt_trapframe)
+      kfree((void*)p->alt_trapframe);
+  p->alt_trapframe = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
